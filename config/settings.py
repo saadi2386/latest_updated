c1354ef5f301e7e6958b151ab539c6f3a1484fdd
saadi2386 <90589353+saadi2386@users.noreply.github.com>
2023-06-08 04:09:41 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!&1*r#d)0@jo#v24&kic$t6&+4%q0r9&22x0&3t5hhy%g1hjc='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -51,6 +51,7 @@ TIME_ZONE = 'UTC'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('pages.urls')), 
@@ -11,6 +14,8 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')), 
     path('accounts/', include('accounts.urls')),
     path('', include('prediction.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 # Serve static and media files during development
